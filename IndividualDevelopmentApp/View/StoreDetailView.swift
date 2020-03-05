be16bc3b22942f10d0d69b8eba22411ed5a0ef4f
Naoki-Yuzu <toaru.programming@gmail.com>
2020-03-05 16:58:15 +0900

    did add view in store detail

--- IndividualDevelopmentApp/View/StoreDetailView.swift
+++ IndividualDevelopmentApp/View/StoreDetailView.swift
@@ -13,10 +13,12 @@ class StoreDetailView: UIView {
     //MARK: - Properties
     var splitView: UIView!
     var splitView2: UIView!
+    var postUserView: UIView!
     var scrollView: UIScrollView!
     var line: OriginalLine!
     var line2: OriginalLine!
     var line3: OriginalLine!
+    var line4: OriginalLine!
     var storeNameLabel: OriginalLabel!
     var storeImageView: OriginalImageView!
     var storeImpresstionTitleLabel: OriginalLabel!
@@ -50,7 +52,14 @@ class StoreDetailView: UIView {
         
         line2.frame = CGRect(x: scrollView.bounds.width * 0.05, y: storeNameLabel.bounds.height + line.bounds.height + storeImageView.bounds.height + 60, width: scrollView.bounds.width * 0.9, height: 0.7)
         
-        splitView2.frame = CGRect(x: scrollView.bounds.width * 0.1, y: storeNameLabel.bounds.height + line.bounds.height + storeImageView.bounds.height + line2.bounds.height + 80 , width: scrollView.bounds.width * 0.8, height: 400)
+        postUserView.frame = CGRect(x: scrollView.bounds.width * 0.1, y: storeNameLabel.bounds.height + line.bounds.height + storeImageView.bounds.height + line2.bounds.height + 80, width: scrollView.bounds.width * 0.8, height: 100)
+        
+        line4.frame = CGRect(x: scrollView.bounds.width * 0.05, y: storeNameLabel.bounds.height + line.bounds.height + storeImageView.bounds.height + postUserView.bounds.height + 100, width: scrollView.bounds.width * 0.9, height: 0.7)
+        
+//        splitView2.frame = CGRect(x: scrollView.bounds.width * 0.1, y: storeNameLabel.bounds.height + line.bounds.height + storeImageView.bounds.height + line2.bounds.height + postUserView.bounds.height + line4.bounds.height + 120 , width: scrollView.bounds.width * 0.8, height: 400)
+        let splitViewOriginY = storeNameLabel.bounds.height + line.bounds.height + storeImageView.bounds.height + line2.bounds.height + postUserView.bounds.height + line4.bounds.height
+        
+        splitView2.frame = CGRect(x: scrollView.bounds.width * 0.1, y: splitViewOriginY + 120, width: scrollView.bounds.width * 0.8, height: 400)
         
         storeImpresstionTitleLabel.frame = CGRect(x: splitView2.bounds.width * 0.15, y: splitView2.bounds.origin.y + 20, width: splitView2.bounds.width * 0.7, height: 20)
         
@@ -64,7 +73,7 @@ class StoreDetailView: UIView {
         storeImpresstionLabel.frame = rect
         
         
-        scrollView.contentSize = CGSize(width: splitView.bounds.width, height: storeNameLabel.bounds.height + line.bounds.height + storeImageView.bounds.height + line2.bounds.height + splitView2.bounds.height + 100)
+        scrollView.contentSize = CGSize(width: splitView.bounds.width, height: splitViewOriginY + splitView2.bounds.height + 140)
         
     }
     
@@ -81,12 +90,16 @@ class StoreDetailView: UIView {
         
         line = OriginalLine(color: .black)
         line2 = OriginalLine(color: .black)
+        line4 = OriginalLine(color: .black)
         
         storeNameLabel = OriginalLabel(textOfLabel: "もふふ", textColor: .black, fontAndSize: .boldSystemFont(ofSize: 20))
         storeNameLabel.textAlignment = .center
         storeNameLabel.adjustsFontSizeToFitWidth = true
         storeImageView = OriginalImageView(withImage: UIImage(named: "store_icon")!, cornerRadius: 3)
         storeImageView.contentMode = .scaleAspectFit
+        
+        postUserView = UIView()
+        postUserView.backgroundColor = .red
         
         splitView2 = UIView()
         splitView2.backgroundColor = .white
@@ -108,7 +121,15 @@ class StoreDetailView: UIView {
         
         storeImpresstionLabel = OriginalLabel(textOfLabel: "のふふもふもふもふもふおふおふもふもふfもふもふいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい", textColor: .black, fontAndSize: .systemFont(ofSize: 15))
         storeImpresstionLabel.numberOfLines = 0
-//        storeImpresstionLabel.backgroundColor = UIColor(red: 250/255, green: 235/255, blue: 215/255, alpha: 1)
+        
+        // Create a new Attributed String
+        let attributedString = NSMutableAttributedString.init(string: storeImpresstionLabel.text!)
+        
+        // Add Underline Style Attribute.
+        attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: 1, range:
+            NSRange.init(location: 0, length: attributedString.length));
+        storeImpresstionLabel.attributedText = attributedString
+
         
         addSubview(splitView)
         splitView.addSubview(scrollView)
@@ -116,6 +137,8 @@ class StoreDetailView: UIView {
         scrollView.addSubview(line)
         scrollView.addSubview(storeImageView)
         scrollView.addSubview(line2)
+        scrollView.addSubview(postUserView)
+        scrollView.addSubview(line4)
         scrollView.addSubview(splitView2)
         splitView2.addSubview(storeImpresstionTitleLabel)
         splitView2.addSubview(line3)
